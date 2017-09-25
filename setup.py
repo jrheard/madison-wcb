55eac5ea7657de8dca225293b5887ab19e4fa455
jrheard <jrheard@cs.stanford.edu>
2017-09-25 16:00:42 -0700

    begin attempt to package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="madison_wcb",
+    version="0.1.0",
+    description="A library that allows users to directly control a WaterColorBot by writing Python code.",
+    url="http://github.com/jrheard/madison_wcb",
+    author="JR Heard",
+    license="MIT",
+    packages=find_packages(),
+)

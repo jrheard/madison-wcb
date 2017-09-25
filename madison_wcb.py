3da3d1e95a82c0af81c4194486b3d1fb6d1953cb
jrheard <jrheard@cs.stanford.edu>
2017-09-25 16:50:09 -0700

    attempt to hide make_cnc_request from docs

--- madison_wcb.py
+++ madison_wcb.py
@@ -15,7 +15,7 @@ state = {
 WCB_WIDTH = 500
 WCB_HEIGHT = 360
 
-def make_cnc_request(endpoint):
+def _make_cnc_request(endpoint):
     if state['connected_to_bot']:
         requests.get('http://localhost:4242/' + endpoint)
 
@@ -48,11 +48,11 @@ def cleanup():
 
 def park():
     """Park the watercolorbot's brush in the top-left corner."""
-    make_cnc_request("park")
+    _make_cnc_request("park")
 
 def wash_brush():
     """Wash the brush in water."""
-    make_cnc_request("pen.wash")
+    _make_cnc_request("pen.wash")
 
 def get_color(index):
     """Dips the brush in paint.
@@ -61,7 +61,7 @@ def get_color(index):
         index - an integer between 0 and 7, inclusive. Tells the bot which color you want.
     """
     if index in range(0, 8):
-        make_cnc_request("tool.color./" + str(index))
+        _make_cnc_request("tool.color./" + str(index))
 
         colors = ["black", "red", "orange", "yellow", "green", "blue", "purple", "brown"]
         state['turtle'].color(colors[index])
@@ -71,7 +71,7 @@ def get_color(index):
 
 def brush_down():
     """Puts the brush in its "down" position, so that it touches the paper."""
-    make_cnc_request("pen.down")
+    _make_cnc_request("pen.down")
     state['turtle'].pendown()
 
     # Wiggle the turtle one step so that it marks a dot on the turtle canvas.
@@ -80,7 +80,7 @@ def brush_down():
 
 def brush_up():
     """Puts the brush in its "up" position, so that it doesn't touch the paper."""
-    make_cnc_request("pen.up")
+    _make_cnc_request("pen.up")
     state['turtle'].penup()
 
 def move_to(x, y):
@@ -90,7 +90,7 @@ def move_to(x, y):
         x - a number between -250 and 250.
         y - a number between -180 and 180.
     """
-    make_cnc_request("coord/{0}/{1}".format(x, y))
+    _make_cnc_request("coord/{0}/{1}".format(x, y))
     state['turtle'].goto(x, y)
 
 def point_in_direction(angle):
@@ -99,7 +99,7 @@ def point_in_direction(angle):
     Arguments:
         angle - a number between 0 and 360.
     """
-    make_cnc_request("move.absturn./" + str(angle))
+    _make_cnc_request("move.absturn./" + str(angle))
     state['turtle'].setheading(90 - angle)
 
 def move_forward(num_steps):
@@ -108,7 +108,7 @@ def move_forward(num_steps):
     Arguments:
         num_steps - a number like 20. A bigger number makes the brush move farther.
     """
-    make_cnc_request("move.forward./" + str(num_steps))
+    _make_cnc_request("move.forward./" + str(num_steps))
     state['turtle'].forward(num_steps)
 
 def turn_left(relative_angle):
@@ -118,7 +118,7 @@ def turn_left(relative_angle):
         relative_angle - a number like 10.
             A bigger number makes the turtle turn farther to the left.
     """
-    make_cnc_request("move.left./" + str(relative_angle))
+    _make_cnc_request("move.left./" + str(relative_angle))
     state['turtle'].left(relative_angle)
 
 def turn_right(relative_angle):
@@ -128,7 +128,7 @@ def turn_right(relative_angle):
         relative_angle - a number like 10.
             A bigger number makes the turtle turn farther to the right.
     """
-    make_cnc_request("move.right./" + str(relative_angle))
+    _make_cnc_request("move.right./" + str(relative_angle))
     state['turtle'].right(relative_angle)
 
 def get_position():

e47606d55dea8e9d09bb09f04cce67ce9bf3eb5c
jrheard <jrheard@cs.stanford.edu>
2017-09-27 17:28:04 -0700

    add long_description for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
 
+long_description="This library's source code lives at https://github.com/jrheard/madison_wcb and its documentation lives at http://madison-wcb.readthedocs.io/."
+
 setup(
     name="madison_wcb",
     version="0.1.0",
     description="A library that allows users to directly control a WaterColorBot by writing Python code.",
+    long_description=long_description,
     url="http://github.com/jrheard/madison_wcb",
     author="JR Heard",
     author_email="jrrrheard@gmail.com",
